d0c6d3b53a990c25a57037cb9eb7e86ce04ab9da
jaLeague <jackie.arseneau@jointheleague.org>
2017-09-07 12:56:46 -0700

    Init

---
+++ src/SleepyHead.java
@@ -0,0 +1,28 @@
+//    Copyright (c) The League of Amazing Programmers 2013-2017
+//    Level 0
+
+import javax.swing.JOptionPane;
+
+
+public class SleepyHead {
+
+
+    public static void main(String[] args) {
+        
+        boolean isWeekday, isVacation;
+        
+        /*
+         * Ask the user for these values using
+         * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
+         * JOptionPane.YES_NO_OPTION);
+         */
+    
+
+
+        /*
+         * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
+         * print “get up lazybones!” If it is a weekday, and we are on vacation,
+         * print “sleep in”.
+         */
+    }
+}
