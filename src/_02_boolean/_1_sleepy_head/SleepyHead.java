5622112d72149a26925e447cbc844edd6566b437
root <root@ip-172-31-21-29.us-west-1.compute.internal>
2020-06-01 21:19:54 -0700

    update from league central

--- src/_02_boolean/_1_sleepy_head/SleepyHead.java
+++ src/_02_boolean/_1_sleepy_head/SleepyHead.java
@@ -1,28 +1,77 @@
 package _02_boolean._1_sleepy_head;
 
 import javax.swing.JOptionPane;
-
+import org.jointheleague.graphical.robot.Robot;
 
 public class SleepyHead {
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        
-        boolean isWeekday, isVacation;
-        
-        /* You MUST use the above boolean variables in your code */
-        
-        /*
-         * Ask the user for these values using a confirm dialog like the one below
-         * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
-         */
-    
+		// boolean variables can only hold one of two values - true or false
 
+		/************************  SLEEPY HEAD  **************************/
+		boolean isWeekend;
+		// Write code to ask the user what day it is.
+        
+		// Set the boolean isWeekend based on the value they enter
+		
+		
+		// If it is the weekend, tell the user they get to sleep in.
+		
+		// If it is not the weekend, tell them to get out of bed and go to school!
+		
+		
+		/************************   STAR STUDENT  **************************/
 
-        /*
-         * Print "sleep in"? if it is a vacation or a weekend. 
-         * If it's a weekday, print "get up lazybones!"? 
-         * If it's a weekday, but we are on vacation,  print "sleep in"?.
-         */
-    }
+		boolean passedExam;
+		// Write code to ask the user what percentage they scored in their last exam
+
+		
+		// If they scored more than 70, they passed the exam.
+		// Set the boolean passedExam based on their score.
+		
+		// If the user passed the exam, congratulate them
+		
+		// otherwise, wish them better luck next time.
+		
+		
+		/************************  GAME OVER  **************************/
+
+		boolean gameIsOver = false;
+		// This code will repeat until gameIsOver is changed to true
+		while (!gameIsOver) {
+			// Ask the user if the game is over. 
+			String gameOver = JOptionPane.showInputDialog("is game over");
+			if (gameOver.equalsIgnoreCase("yes"))
+				gameIsOver = true;
+			
+			// If they answer "yes", change gameIsOver to true
+
+		}
+		// Tell the user "game is over" 
+		
+		
+		/***********************   RED SQUARE  ********************/
+		
+		boolean isRed;
+		// Ask the user what color to draw with. Based on their answer, set the isRed variable
+		
+		
+		boolean isSquare;		
+		// Now ask the user what shape to draw. Based on their answer, set the isSquare variable
+		
+		
+		// Now you MUST use the && operator to join the booleans for this code. 
+		// Ask your teacher if you are not sure how
+		// Use an if statement to ONLY draw a red square when it has been requested (use the method below)
+		// otherwise, tell the user you don't know how to draw that shape
+		
+
+	}
+
+	
+	static void drawRedSquare() {
+		Robot rob = new Robot();
+		// Complete the rest of this method
+	}	
 }
